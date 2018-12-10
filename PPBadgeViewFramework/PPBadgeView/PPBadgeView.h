d3bda173a8052c29acff724d9b42299ee2424204
Lee Arromba <larromba@gmail.com>
2018-12-10 15:43:01 +0000

    + framewok project

---
+++ PPBadgeViewFramework/PPBadgeView/PPBadgeView.h
@@ -0,0 +1,18 @@
+//
+//  PPBadgeView.h
+//  PPBadgeView
+//
+//  Created by Lee Arromba on 10/12/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PPBadgeView.
+FOUNDATION_EXPORT double PPBadgeViewVersionNumber;
+
+//! Project version string for PPBadgeView.
+FOUNDATION_EXPORT const unsigned char PPBadgeViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPBadgeView/PublicHeader.h>
+
+
